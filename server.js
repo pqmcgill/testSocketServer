3d42c2078b0babc5b395326d789880ab31917aee
pqmcgill <pqmcgill@gmail.com>
2016-11-15 09:16:36 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const app = require('http').createServer();
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ server: app });
+
+var count = 0;
+
+app.listen(4003, () => {
+	console.log('server listening on port 4003');
+});
+
+wss.on('connection', ws => {
+	console.log('connected!');
+	setInterval(() => {
+    console.log('sending data');
+    ws.send(JSON.stringify({
+      event: 'notification',
+      id: `${count}`,
+      recipient: '3644bfa9-5d4e-45aa-9ca8-04199de838d1',
+      description: 'shh, super secret message',
+      viewed: false,
+      linkableObject: {
+        typekey: 'designReview',
+        contextId: '7a65022d-72cb-4c3c-a6ef-aec296656011'
+      },
+      contextObject: {
+        tab: 'history',
+        designReviewType: 'userNeedReview'
+      },
+      creationDate: Date.now()
+    }));
+    count+=1;
+	}, 5000);
+});
