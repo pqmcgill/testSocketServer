f3991834eb83e73e852296e60bcdbe9e90aa541b
pqmcgill <pqmcgill@gmail.com>
2016-11-16 09:49:17 -0500

    updated model

--- server.js
+++ server.js
@@ -12,22 +12,30 @@ wss.on('connection', ws => {
 	console.log('connected!');
 	setInterval(() => {
     console.log('sending data');
-    ws.send(JSON.stringify({
-      event: 'notification',
-      id: `${count}`,
-      recipient: '3644bfa9-5d4e-45aa-9ca8-04199de838d1',
-      description: 'shh, super secret message',
-      viewed: false,
-      linkableObject: {
-        typekey: 'designReview',
-        contextId: '7a65022d-72cb-4c3c-a6ef-aec296656011'
-      },
-      contextObject: {
-        tab: 'history',
-        designReviewType: 'userNeedReview'
-      },
-      creationDate: Date.now()
-    }));
+    ws.send(JSON.stringify(
+      
+      
+      {
+        type: 'internalNotification',
+        data: {
+          id: `${count}`,
+          recipient: '3644bfa9-5d4e-45aa-9ca8-04199de838d1',
+          description: 'shh, super secret message',
+          viewed: false,
+          linkableObject: {
+            typeKey: 'designReview',
+            contextId: '7a65022d-72cb-4c3c-a6ef-aec296656011'
+          },
+          contextObject: {
+            tab: 'history',
+            designReviewType: 'userNeedReview'
+          },
+          creationDate: Date.now()
+        }
+      }
+    
+    
+    ));
     count+=1;
 	}, 5000);
 });
